43e235005ba0f122504601149c3a0017cfd5525b
Jesús Rodríguez Heras <jesusrh1997@gmail.com>
2019-04-29 15:01:34 +0200

    Añadido el script Estado.sh

---
+++ Archivos/Estado.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DIR_TO_CHECK='/home/jesus/Documentos/Pruebas/Ficheros' #Directorio que queremos inspeccionar
+
+OLD_STAT_FILE='/home/jesus/Documentos/Pruebas/Viejo.txt' #Fichero "hash" con el directorio antiguo
+
+if [ -e $OLD_STAT_FILE ]
+then
+        OLD_STAT=`cat $OLD_STAT_FILE`
+else
+        OLD_STAT="nothing"
+fi
+
+NEW_STAT=`stat -t $DIR_TO_CHECK`
+
+if [ "$OLD_STAT" != "$NEW_STAT" ]
+then
+        echo 'Directory has changed. Do something!'
+        
+        #Si ha habido algún cambio en el directorio a inspeccionar te dice esto y aquí podemos hacer lo que sea, por ejemplo el mandar el fichero que queramos a la fpga para que haga lo que tenga que hacer y luego ir haciendo comprobaciones.
+        # do whatever you want to do with the directory.
+        #Podemos lanzarlo con el comando: watch -n 1 ./Estado.sh y cada segundo, comprobará si ha cambiado el directorio "Ficheros" y en función de eso, podremos programar la fpga o lanzar el scp
+        
+        # update the OLD_STAT_FILE
+        
+        echo $NEW_STAT > $OLD_STAT_FILE
+fi
