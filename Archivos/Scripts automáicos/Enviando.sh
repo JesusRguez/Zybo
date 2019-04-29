89412cf90f1500556fe023f63576ca3eec8ad1b2
Jesús Rodríguez Heras <jesusrh1997@gmail.com>
2019-04-30 00:00:36 +0200

    Automatización completa pero a lo bestia

--- Archivos/Scripts automáicos/Enviando.sh
+++ Archivos/Scripts automáicos/Enviando.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 
-DIR_TO_CHECK=$PWD/recibir #Directorio que queremos inspeccionar
+DIR_TO_CHECK=$PWD/enviar #Directorio que queremos inspeccionar
 
-OLD_STAT_FILE=$PWD/backups/Viejo.txt #Fichero "hash" con el directorio antiguo
+#DIR_TO_SEND=$PWD/trabajo #Directorio donde enviar
+
+OLD_STAT_FILE=$PWD/backups/ViejoEnviar.txt #Fichero "hash" con el directorio antiguo
 
 if [ -e $OLD_STAT_FILE ]
 then
@@ -17,12 +19,25 @@ NEW_STAT=`stat -t $DIR_TO_CHECK`
 if [ "$OLD_STAT" != "$NEW_STAT" ]
 then
         echo 'Directory has changed. Do something!'
-        
+
         #Si ha habido algún cambio en el directorio a inspeccionar te dice esto y aquí podemos hacer lo que sea, por ejemplo el mandar el fichero que queramos a la fpga para que haga lo que tenga que hacer y luego ir haciendo comprobaciones.
         # do whatever you want to do with the directory.
         #Podemos lanzarlo con el comando: watch -n 1 ./Estado.sh y cada segundo, comprobará si ha cambiado el directorio "Ficheros" y en función de eso, podremos programar la fpga o lanzar el scp
-        
+
         # update the OLD_STAT_FILE
-        
+
+	cd $DIR_TO_CHECK
+
+	n=$(ls | wc -l)
+	cero=0
+
+	if [[ n -ne cero ]]
+	then
+		fichero=$(ls -t | head -1) #Obtenemso el fichero más reciente
+		#echo $fichero
+		echo Copiado el fichero $fichero a $DIR_TO_SEND
+		sshpass -p 1997 scp $fichero jesus@192.168.1.39:/home/jesus/Vídeos
+	fi
+
         echo $NEW_STAT > $OLD_STAT_FILE
 fi
